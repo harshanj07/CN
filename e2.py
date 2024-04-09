4d9563ed77414d19a7e7a91167d24bc8e6341023
harshanj07 <166501004+harshanj07@users.noreply.github.com>
2024-04-09 23:42:19 +0530

    2.py

--- e2.py
+++ e2.py
@@ -1,61 +1,54 @@
-def two_dimensional_parity_check():
-  bits = int(input("Enter number of bits :"))
-  count = int(input("Enter the number of datas to be transmitted:"))
-  par = input('Enter parity(odd(o)/even(e)) :')
-  data = []
-
-  for i in range(count):
-    a = input('Enter the data :')
-    data.append(a)
-
-  for i in range(count):
-    data[i] = make_parity(data[i],par)
-
-  last = ''
-  s = ''
-#to find the parity for each columns
-  for i in range(bits):
-    for j in range(count):
-      s += data[j][i]
-    if par == 'o':
-      last = (last +'1') if s.count('1')%2==0 else (last +'0')
+def singel_par(data,p):
+    c=data.count('1')
+    if p==1:
+        if c%2==1:
+            data.append('0')
+        else:
+            data.append('1')
     else:
-      last = (last+'1') if s.count('1')%2==1 else (last + '0')
-    s = ''
-  data.append(make_parity(last,par))
-  print(data)
+        if c%2==0:
+            data.append('0')
+        else:
+            data.append('1')
+    return data
+def double_par(data,l,p):
+    colpar=[]
+    for i in range(l):
+        c=0
+        for j in data:
+            if j[i]=='1':
+                c=c+1
+        if p==1:
+            if c%2==1:
+                colpar.append('0')
+            else:
+                colpar.append('1')
+        else:
+            if c%2==0:
+                colpar.append('0')
+            else:
+                colpar.append('1')
+                
+    colpar=singel_par(colpar,p)
+    data.append(colpar)
+    return data
 
-  rx_data = []
-  for i in range(len(data)):
-    a = input('Enter the recieved data :')
-    rx_data.append(a)
-
-  changed = False
-  #s = ''
-  for i in range(count):
-    if not check_parity_og(rx_data[i],par):
-      print('The transmitted data is incorrect.')
-      break
-  if not changed:
-    print('The transmitted data is correct.')
-
-def check_parity_og(data, par):
-  if par=='o':
-    return data.count('1')%2 ==1
-  else:
-    return data.count('1')%2 ==0
-
-def make_parity(data, par) :
-  if par == 'o':
-    data = (data+'1') if data.count('1')%2==0 else (data+'0')
-  else:
-    data = (data+'1') if data.count('1')%2==1 else (data + '0')
-  return data
-
-def main():
-    print("\tERROR DETECTION METHODS")
-    print("Two-Dimensional Parity Check\n")
-    two_dimensional_parity_check()
-main()
-
-
+n=int(input("Enter the no of data:"))
+l=int(input("Enter the length of each frame:"))
+print("\n1.Odd Parity\n2.Even Parity")
+p=int(input("Enter the Parity:"))
+data=[]
+for i in range(n):
+    d=list(input("Enter the data:"))
+    d=singel_par(d,p)
+    data.append(d)
+data=double_par(data,l,p)
+print(data)
+rx=[]
+for i in range(n+1):
+    r=list(input("Enter the received data:"))
+    rx.append(r)
+if data==rx:
+    print("Success")
+else:
+    print("Wrong")
